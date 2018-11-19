59026c716b979b65eb227f08813d7172c81e07e6
Horváth Dániel <h.danika07@gmail.com>
2018-11-19 21:15:34 +0100

    Add files via upload

---
+++ ev3starwars.py
@@ -0,0 +1,27 @@
+from ev3dev2.sound import Sound
+
+sound = Sound()
+sound.speak('A long time ago in a galaxy far')
+sound.speak('far away')
+
+sound.play_song((
+('D4', 'e3'), # intro anacrouse
+('D4', 'e3'),
+('D4', 'e3'),
+('G4', 'h'), # meas 1
+('D5', 'h'),
+('C5', 'e3'), # meas 2
+('B4', 'e3'),
+('A4', 'e3'),
+('G5', 'h'),
+('D5', 'q'),
+('C5', 'e3'), # meas 3
+('B4', 'e3'),
+('A4', 'e3'),
+('G5', 'h'),
+('D5', 'q'),
+('C5', 'e3'), # meas 4
+('B4', 'e3'),
+('C5', 'e3'),
+('A4', 'h.'),
+))
