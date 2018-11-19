5fc3fc622a89389dbb6d8386c9999dbeb3854bd6
Horváth Dániel <h.danika07@gmail.com>
2018-11-19 21:42:32 +0100

    Add files via upload

--- ev3helloWorld.py
+++ ev3helloWorld.py
@@ -1,2 +1,3 @@
 import ev3dev2.fonts as fonts
-display.draw.text((10,10), 'Hello World!', font=fonts.load('luBS14'))
+from ev3dev2.display import Display
+Display.draw.text((10,10), 'Hello World!', font=fonts.load('luBS14'))
